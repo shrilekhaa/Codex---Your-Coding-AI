96f8194efb166bc4be2111e91bd68e6f521f2c16
Shrilekha <shriraghav18@gmail.com>
2022-12-27 17:06:40 +0530

    Updated the server url

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
     
     // Fetch data
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-your-coding-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': "application/json"
